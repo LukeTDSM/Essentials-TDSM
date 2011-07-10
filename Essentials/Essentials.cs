a860b1f1a1b8fd14454f4c79b08edc1c19203d6f
Jake Askitis <jakeydev@gmail.com>
2011-07-11 07:29:57 +1000

    Git was giving me a hard time, so just a re-commit :(

--- Essentials/Essentials.cs
+++ Essentials/Essentials.cs
@@ -17,13 +17,13 @@ namespace Essentials
     public class Essentials : Plugin
     {
         /*
-         * @Developers
-         * 
-         * Plugins need to be in .NET 3.5
-         * Otherwise TDSM will be unable to load it. 
-         * 
-         * As of June 16, 1:15 AM, TDSM should now load Plugins Dynamically.
-         */
+* @Developers
+*
+* Plugins need to be in .NET 3.5
+* Otherwise TDSM will be unable to load it.
+*
+* As of June 16, 1:15 AM, TDSM should now load Plugins Dynamically.
+*/
 
         // tConsole is used for when logging Output to the console & a log file.
 
@@ -92,7 +92,7 @@ namespace Essentials
             {
                 if (commands[0] != null && commands[0].Trim().Length > 0) //If it is not nothing, and the string is actually something
                 {
-                	//Last command COMMAND!
+                 //Last command COMMAND!
                     if (commands[0].Equals("/!"))
                     {
                         PlayerCommandEvent lastEvent = null;
@@ -139,7 +139,7 @@ namespace Essentials
                             {
                                 Player targetPlayer = Program.server.GetPlayerByName(commands[1]);
                                 NetMessage.SendData(26, -1, -1, " of unknown causes...", targetPlayer.whoAmi, 0, (float)9999, (float)0);
-                                Event.Player.sendMessage("OMG!  You killed " + commands[1] + "!", 255, 0f, 255f, 255f);
+                                Event.Player.sendMessage("OMG! You killed " + commands[1] + "!", 255, 0f, 255f, 255f);
                                 Log("Player " + Event.Player + " used /slay on " + targetPlayer.getName());
                             }
                             catch (NullReferenceException)
@@ -152,123 +152,147 @@ namespace Essentials
                     //GOD COMMAND!
                     if (commands[0].Equals("/god"))
                     {
-                    	if (!Event.Player.Op)
-                    	{
-                    		Event.Player.sendMessage("Error: you must be an Op to use /god");
-                    	}
-                    	else
-                    	{
-                        	Event.Player.setGodMode(true);
-                        	Event.Player.sendMessage("You are a GOD!");	
-                    	}
-                        	Event.Cancelled = true;
+                     if (!Event.Player.Op)
+                     {
+                     Event.Player.sendMessage("Error: you must be an Op to use /god");
+                     }
+                     else
+                     {
+                         Event.Player.setGodMode(true);
+                         Event.Player.sendMessage("You are a GOD!");
+                     }
+                         Event.Cancelled = true;
                         }
                     }
                 
-                	//HEAL COMMAND!
-                	if (commands[0].Equals("/heal"))
-                	{
-                		if (!Event.Player.Op)
-                		{
-                			Event.Player.sendMessage("Error: you must be an Op to use /heal");
-                		}
-                		else if (commands.Length < 2)
-                		{
-                			
-                			Player player = Event.Player;
-                			
-                			Event.Player.sendMessage("You did not specify the player, so you were healed");
-                			
-                			
-                			for(i = 0; i < 20; i++)
-                			{
-                			Item.NewItem((int)player.Position.X, (int)player.Position.Y, player.width, player.height, 58, 1, false);
-                			}
-                		}
-                		else
-                		{ 
-                			try
-                			{
-                			Player player = Program.server.GetPlayerByName(commands[1]);;
-                			
-                			for(i = 0; i < 20; i++)
-                			{
-                			Item.NewItem((int)player.Position.X, (int)player.Position.Y, player.width, player.height, 58, 1, false);
-                			}
-                			
-                			Event.Player.sendMessage("You have healed that player!");
-                			}
-                			catch (NullReferenceException)
+                 //HEAL COMMAND!
+                 if (commands[0].Equals("/heal"))
+                 {
+                 if (!Event.Player.Op)
+                 {
+                 Event.Player.sendMessage("Error: you must be an Op to use /heal");
+                 }
+                 else if (commands.Length < 2)
+                 {
+                
+                 Player player = Event.Player;
+                
+                 Event.Player.sendMessage("You did not specify the player, so you were healed");
+                
+                
+                 for(i = 0; i < 20; i++)
+                 {
+                 Item.NewItem((int)player.Position.X, (int)player.Position.Y, player.width, player.height, 58, 1, false);
+                 }
+                 }
+                 else
+                 {
+                 try
+                 {
+                 Player player = Program.server.GetPlayerByName(commands[1]);;
+                
+                 for(i = 0; i < 20; i++)
+                 {
+                 Item.NewItem((int)player.Position.X, (int)player.Position.Y, player.width, player.height, 58, 1, false);
+                 }
+                
+                 Event.Player.sendMessage("You have healed that player!");
+                 }
+                 catch (NullReferenceException)
                             {
                                 Event.Player.sendMessage("Error: Player not online.");
                             }
-                		}
-                		
-                		Event.Cancelled = true;
-                	}
-                	
-                	//Ping! Command!
-                	if (commands[0].Equals("/ping"))
-                	{
-                		Event.Cancelled = true;
-                	}
-                	
-                	//SUICIDE COMMAND!
-                	if (commands[0].Equals("/suicide"))
-                	{
-                		Player Suicide = Event.Player;
-                		if (!Event.Player.Op)
-                		{
-                			Event.Player.sendMessage("Error: you must be Op to use /suicide");
-                		}
-                		else
-                		{
-                			NetMessage.SendData(26, -1, -1, " commited suicide!", Suicide.whoAmi, 0, (float)9999, (float)0);
-                		}
-                	}
-                	
-                	//Kits!
-                	if (commands[0].Equals("/kit"))
-                	{
-                		//Have to be op
-                		if (!Event.Player.Op)
-                		{
-                			Event.Player.sendMessage("Error: you must be Op to use /kit");
-                		}
-                		
-						if (commands.Length > 1)
-						{
-                			//Admin KIT
-                			if (commands[1].Equals("admin"))
-                			{
-                			Event.Player.sendMessage("You have recieved the Admin kit.");
-                			}
-                		
-                			//BUILDER KIT
-                			if (commands[1].Equals("builder"))
-                			{
-                			Event.Player.sendMessage("You have recieved the Builder kit.");
-                			}
-                		
-                			//Mod KIT
-                			if (commands[1].Equals("mod"))
-                			{
-                			Event.Player.sendMessage("You have recieved the Mod kit.");
-                			}
-                		
-                			//Help ::: Shows what kits there are
-                			if (commands[1].Equals("help"))
-                			{
-                			Event.Player.sendMessage("The kits are: admin, builder and mod!");
-                			}
-						}
-                		
-                		//Error message
-                		if (commands.Length < 1)
+                 }
+                
+                 Event.Cancelled = true;
+                 }
+                
+                 //Ping! Command!
+                    if (commands[0].Equals("/ping") || commands[0].Equals("/pong"))
+                    {
+                        if ((commands.Length > 1 && (commands[1].ToLower().Equals("ping") || commands[1].ToLower().Equals("pong"))) || commands.Length < 2)
+                        {
+                            commands[0] = commands[0].Remove(0, 1);
+                            string message = "";
+                            for (int i = 0; i < commands.Length; i++)
+                            {
+                                if (commands[i].ToLower().Equals("ping"))
+                                {
+                                    message += "pong ";
+                                }
+                                else if (commands[i].ToLower().Equals("pong"))
+                                {
+                                    message += "ping ";
+                                }
+                            }
+                            message = message.Trim() + "!";
+                            Event.Player.sendMessage(message);
+                        }
+                        else if (commands.Length > 1)
+                        {
+                            Event.Player.sendMessage("This is ping pong! There ain't no room for " + commands[1] + "!");
+                        }
+                        Event.Cancelled = true;
+                    }
+                
+                 //SUICIDE COMMAND!
+                 if (commands[0].Equals("/suicide"))
+                 {
+                 Player Suicide = Event.Player;
+                 if (!Event.Player.Op)
+                 {
+                 Event.Player.sendMessage("Error: you must be Op to use /suicide");
+                 }
+                 else
+                 {
+                 NetMessage.SendData(26, -1, -1, " commited suicide!", Suicide.whoAmi, 0, (float)9999, (float)0);
+                 }
+                 }
+                
+                 //Kits!
+                 if (commands[0].Equals("/kit"))
+                 {
+                        Event.Cancelled = true;
+                 //Have to be op
+                 if (!Event.Player.Op)
+                 {
+                 Event.Player.sendMessage("Error: you must be Op to use /kit");
+                            return;
+                 }
+                
+if (commands.Length > 1)
+{
+                 //Admin KIT
+                 if (commands[1].Equals("admin"))
+                 {
+                 Event.Player.sendMessage("You have recieved the Admin kit.");
+                 }
+                 //BUILDER KIT
+                 else if (commands[1].Equals("builder"))
+                 {
+                 Event.Player.sendMessage("You have recieved the Builder kit.");
+                 }
+                 //Mod KIT
+                 else if (commands[1].Equals("mod"))
+                 {
+                 Event.Player.sendMessage("You have recieved the Mod kit.");
+                 }
+                 //Help ::: Shows what kits there are
+                            else if (commands[1].Equals("help"))
+                            {
+                                Event.Player.sendMessage("The kits are: admin, builder and mod!");
+                            }
+                            else
+                            {
+                                Event.Player.sendMessage("Error: specified kit " + commands[1] + " does not exist.");
+                            }
+}
+                 //Error message
+                 else
                         {
                             Event.Player.sendMessage("Error: You did not specify a kit!");
                         }
-                	}
+                 }
             }
         }
 
